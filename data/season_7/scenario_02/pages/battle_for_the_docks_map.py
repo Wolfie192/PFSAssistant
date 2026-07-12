52820ac6dda31bb868bb724fc93e96b68a0b811c
Omen (Wolfie) <56147209+Wolfie192@users.noreply.github.com>
2026-07-11 21:54:46 -0700

    Delete data/season_7/scenario_02 directory

--- data/season_7/scenario_02/pages/battle_for_the_docks_map.py
+++
@@ -1,6 +0,0 @@
-def battle_for_the_docks_map():
-    pass
-
-
-if __name__ == "__main__":
-    battle_for_the_docks_map()
--- data/season_7/scenario_02/pages/roster.py
+++
@@ -1,9 +0,0 @@
-from src.RosterModel import Roster
-
-
-def roster():
-    Roster()
-
-
-if __name__ == "__main__":
-    roster()
